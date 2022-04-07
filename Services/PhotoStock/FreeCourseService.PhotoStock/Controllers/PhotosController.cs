a231a1352f29ff7fa116b415ebead0ea0d0e4fbb
tuncer\tuncerdirlik <tuncerdirlik@hotmail.com>
2022-04-07 23:43:41 +0300

    photostock api completed

---
+++ Services/PhotoStock/FreeCourseService.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,57 @@
+﻿using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Dtos;
+using FreeCourse.Shared.Enums;
+using FreeCourseService.PhotoStock.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreeCourseService.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if (photo != null && photo.Length > 0)
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "photos", photo.FileName);
+
+                using var stream = new FileStream(path, FileMode.Create);
+                await photo.CopyToAsync(stream, cancellationToken);
+
+                var returnPath = $"photos/{photo.FileName}";
+
+                PhotoDto photoDto = new()
+                {
+                    Url = returnPath
+                };
+
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, (int)ResponseStatusCodes.Ok));
+
+            }
+            else
+            {
+                return CreateActionResultInstance(Response<PhotoDto>.Fail("photo is empty", (int)ResponseStatusCodes.BadRequest));
+            }
+        }
+
+        [HttpDelete("{photoUrl}")]
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "photos", photoUrl);
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("photo not found", (int)ResponseStatusCodes.NotFound));
+            }
+
+            System.IO.File.Delete(path);
+
+            return CreateActionResultInstance(Response<NoContent>.Success((int)ResponseStatusCodes.NoContent));
+        }
+    }
+}
