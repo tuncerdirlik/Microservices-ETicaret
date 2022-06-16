a346f120050574dfc0f89f2eed397b5d3e351e3f
tuncer\tuncerdirlik <tuncerdirlik@hotmail.com>
2022-06-16 23:26:09 +0300

    token exchange

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -77,9 +77,7 @@ namespace FreeCourse.IdentityServer
                     AllowedScopes =
                     {
                         "basket_fullpermission",
-                        "discount_fullpermission",
                         "order_fullpermission",
-                        "payment_fullpermission",
                         "gateway_fullpermission",
                         IdentityServerConstants.StandardScopes.Email,
                         IdentityServerConstants.StandardScopes.OpenId,
@@ -92,6 +90,19 @@ namespace FreeCourse.IdentityServer
                     RefreshTokenExpiration = TokenExpiration.Absolute,
                     AbsoluteRefreshTokenLifetime = (int)(DateTime.Now.AddDays(60) - DateTime.Now).TotalSeconds,
                     RefreshTokenUsage = TokenUsage.ReUse
+                },
+                new Client
+                {
+                    ClientName = "Token Exchange Client",
+                    ClientId = "TokenExchangeClient",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = new []{"urn:ietf:params:oauth:grant-type:token-exchange" },
+                    AllowedScopes =
+                    {
+                        "discount_fullpermission",
+                        "payment_fullpermission",
+                         IdentityServerConstants.StandardScopes.OpenId,
+                    }
                 }
             };
     }
